c4f2721df3a402f880e4ac15975bd6bdae9e1b8b
Markus Pichler <markus.pichler@tugraz.at>
2023-07-28 00:21:02 +0200

    minor example edit

--- examples/example_read_lid_report.py
+++ examples/example_read_lid_report.py
@@ -1,8 +1,11 @@
+import pandas as pd
+
 from swmm_api.report_file.lid_rpt import read_lid_report
 
 
 def main():
-    df = read_lid_report(r'examples\epaswmm5_apps_manual\Example6-Final_AllSections_GUI\Example6-Final_AllSections_GUI_S5_LID_report.txt')
+    df = read_lid_report(r'epaswmm5_apps_manual\Example6-Final_AllSections_GUI\Example6-Final_AllSections_GUI_S5_LID_report.txt')
+    elapsed_time = pd.to_timedelta(df['Elapsed_Time_Hours'], unit='h')
     print(df)
 
 
